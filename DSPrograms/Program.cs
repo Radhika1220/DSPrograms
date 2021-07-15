dabb767a8e1fb8c931656305b10114e4f88715d8
Radhika <radhika.shankar1220@gmail.com>
2021-07-15 18:17:16 +0530

    [Radhika] Balancing Parantheses using stack operations

--- DSPrograms/Program.cs
+++ DSPrograms/Program.cs
@@ -9,6 +9,7 @@ namespace DSPrograms
             Console.WriteLine("Data Structure Programs!");
             Console.WriteLine("1.Unordered List");
             Console.WriteLine("2.Ordered List");
+            Console.WriteLine("3.Balancing Parantheses");
             int check = Convert.ToInt32(Console.ReadLine());
             switch(check)
             {
@@ -18,8 +19,25 @@ namespace DSPrograms
                 case 2:
                     OrderedList.Ordered();
                     break;
+                case 3:
+                    char[] expression = "(5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/(4+3)".ToCharArray();
+                    BalancingParantheses.StackOperations stackOperations = new BalancingParantheses.StackOperations();
+                    bool result=stackOperations.Isbalanced(expression);
+                    if(result)
+                    {
+                        Console.WriteLine("Parantheses Balanced");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parantheses not Balanced");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
 
             }
         }
+       
     }
 }
